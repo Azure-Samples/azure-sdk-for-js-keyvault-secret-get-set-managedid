d988a98cfcafc7359d694ddc9e179b602f9af19a
FredGao-new <fred3332@hotmail.com>
2019-11-12 10:08:55 +0800

    Update index.js

--- key-vault-node-quickstart-v4/index.js
+++ key-vault-node-quickstart-v4/index.js
@@ -1,12 +1,6 @@
 var http = require('http');
 const {DefaultAzureCredential} = require('@azure/identity');
 const {SecretsClient} = require('@azure/keyvault-secrets');
-
-var server = http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-});
-
-async function main(){
 // DefaultAzureCredential expects the following three environment variables:
 // - AZURE_TENANT_ID: The tenant ID in Azure Active Directory
 // - AZURE_CLIENT_ID: The application (client) ID registered in the AAD tenant
@@ -17,14 +11,19 @@ const vaultName = process.env["KEYVAULT_NAME"] || "<YourVaultName>";
 const url = `https://${vaultName}.vault.azure.net`;
   
 const client = new SecretsClient(url, credential);
-  
-  // Create a secret
-  const secretName = "MySecretName";
-  const result = await client.setSecret(secretName, "MySecretValue");
-  console.log("Secret name: ", result.name);
-  // Read the secret we created
-  const secret = await client.getSecret(secretName);
-  console.log("Successfully retrieved 'MySecretName':", secret.value);
+// Create a secret
+const secretName = "MySecretName";
+
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+});
+
+async function main(){
+    const result = await client.setSecret(secretName, "MySecretValue");
+    console.log("Secret name: ", result.name);
+    // Read the secret we created
+    const secret = await client.getSecret(secretName);
+    console.log("Successfully retrieved 'MySecretName':", secret.value);
 }
 
 main().catch((err) => {
