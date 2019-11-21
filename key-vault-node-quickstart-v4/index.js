c7ec99e485fb4e77a471499689b4091ed0f3f521
FredGao-new <fred3332@hotmail.com>
2019-11-21 09:51:34 +0800

    Update index.js

--- key-vault-node-quickstart-v4/index.js
+++ key-vault-node-quickstart-v4/index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 const {DefaultAzureCredential} = require('@azure/identity');
-const {SecretsClient} = require('@azure/keyvault-secrets');
+const {SecretClient} = require('@azure/keyvault-secrets');
 // DefaultAzureCredential expects the following three environment variables:
 // - AZURE_TENANT_ID: The tenant ID in Azure Active Directory
 // - AZURE_CLIENT_ID: The application (client) ID registered in the AAD tenant
@@ -10,7 +10,7 @@ const credential = new DefaultAzureCredential();
 const vaultName = process.env["KEYVAULT_NAME"] || "<YourVaultName>";
 const url = `https://${vaultName}.vault.azure.net`;
   
-const client = new SecretsClient(url, credential);
+const client = new SecretClient(url, credential);
 
 const secretName = "MySecretName";
 
